3c1054ab6d1249ce42f27ef42e12475d9823abe0
mabdullahsoyturk <msoyturk20@ku.edu.tr>
2021-05-07 16:22:45 +0300

    improve semantics

--- peer.go
+++ peer.go
@@ -14,9 +14,7 @@ import (
 
 var peerIDs []string
 var peerConnections []*rpc.Client
-var myID string
-var myPort string
-var myIP string
+var myID, myPort, myIP string
 var senderSequenceNumber int = 0
 
 type Message struct {
@@ -27,90 +25,17 @@ type Message struct {
 
 type MessengerAPI int
 
-func (m *MessengerAPI) MessagePost(message *Message, reply *int) error {
-	fmt.Println((*message).SID + ":\t" + (*message).Transcript + "\t(" + strconv.Itoa((*message).TSM) + ")")
-	*reply = 1
-	return nil
-}
-
-func connectMessenger() {
-	i := 0
-	for {
-		client, err := rpc.Dial("tcp", peerIDs[i]) // Try to connect to the ith peer
-		if err == nil {
-			fmt.Printf("Connected to %s\n", peerIDs[i])
-			peerIDs = append(peerIDs[:i], peerIDs[i+1:]...) // Remove the connected peer from peers
-			peerConnections = append(peerConnections, client)
-		} else {
-			fmt.Println("Couldn't connect" + peerIDs[i] + " trying next peer")
-		}
-
-		if len(peerIDs) > 0 {
-			i = (i + 1) % len(peerIDs) // if there is a next peer, try to connect it
-		} else {
-			break // We are now connected to all pairs.
-		}
-
-		time.Sleep(1000 * time.Millisecond) // Try every second
-	}
-	fmt.Printf("Connected to all peers\n")
-
-	scanner := bufio.NewScanner(os.Stdin)
-	var reply int
-	for {
-		scanner.Scan()
-		input := scanner.Text()
-
-		// Create the message
-		senderSequenceNumber++
-		msg := Message{input, myID, senderSequenceNumber}
-		fmt.Println(msg.SID + ":\t" + msg.Transcript + "\t(" + strconv.Itoa(msg.TSM) + ")") // print my own message
-
-		// Multicast the message
-		for _, client := range peerConnections {
-			err := client.Call("MessengerAPI.MessagePost", msg, &reply)
-			checkError(err)
-		}
-	}
-}
-
-func main() {
-	if len(os.Args) != 2 {
-		fmt.Println("Usage: ", os.Args[0], "myId")
+func checkError(err error) {
+	if err != nil {
+		fmt.Println(err.Error())
 		os.Exit(1)
 	}
-	givenId := os.Args[1]
-
-	readPeers("group.txt", givenId)
-
-	// Create MessengerAPI
-	api := new(MessengerAPI)
-	rpc.Register(api)
-
-	// Start the server
-	listener, err := net.Listen("tcp", myID)
-	checkError(err)
-	fmt.Println("> Service is running!")
-
-	// Connect to Peers
-	go connectMessenger()
-
-	for {
-		conn, err := listener.Accept() // Wait for connections
-		if err != nil {
-			continue
-		}
-		go rpc.ServeConn(conn)
-	}
-
 }
 
 // Modified version of https://stackoverflow.com/questions/8757389/reading-a-file-line-by-line-in-go/16615559#16615559
 func readPeers(filename string, givenId string) {
 	file, err := os.Open(filename)
-	if err != nil {
-		log.Fatal(err)
-	}
+	checkError(err)
 	defer file.Close()
 
 	scanner := bufio.NewScanner(file)
@@ -132,9 +57,74 @@ func readPeers(filename string, givenId string) {
 	}
 }
 
-func checkError(err error) {
-	if err != nil {
-		fmt.Println("Fatal error ", err.Error())
+func (m *MessengerAPI) MessagePost(message *Message, reply *int) error {
+	fmt.Println((*message).SID + ":\t" + (*message).Transcript + "\t(" + strconv.Itoa((*message).TSM) + ")") // Print received message
+	*reply = 1
+	return nil
+}
+
+func connectMessenger() {
+	i := 0
+	for {
+		client, err := rpc.Dial("tcp", peerIDs[i]) // Try to connect to the ith peer
+		if err == nil {
+			fmt.Println("Connected to peer:", peerIDs[i])
+			peerIDs = append(peerIDs[:i], peerIDs[i+1:]...) // Remove the connected peer from peers (https://stackoverflow.com/questions/37334119/how-to-delete-an-element-from-a-slice-in-golang)
+			peerConnections = append(peerConnections, client)
+		} else {
+			fmt.Println("Couldn't connect" + peerIDs[i] + " trying next peer")
+		}
+
+		if len(peerIDs) > 0 {
+			i = (i + 1) % len(peerIDs) // if there is a next peer, try to connect it
+		} else {
+			break // We are now connected to all pairs.
+		}
+
+		time.Sleep(1000 * time.Millisecond) // Try every second
+	}
+	fmt.Println("Connected to all peers")
+
+	scanner := bufio.NewScanner(os.Stdin)
+	var reply int
+	for {
+		scanner.Scan()
+		input := scanner.Text()
+
+		senderSequenceNumber++
+		msg := Message{input, myID, senderSequenceNumber}                                   // Create the message
+		fmt.Println(msg.SID + ":\t" + msg.Transcript + "\t(" + strconv.Itoa(msg.TSM) + ")") // Print my message to the console
+
+		for _, client := range peerConnections { // Multicast my message
+			err := client.Call("MessengerAPI.MessagePost", msg, &reply)
+			checkError(err)
+		}
+	}
+}
+
+func main() {
+	if len(os.Args) != 2 {
+		fmt.Println("Usage: ", os.Args[0], "myId")
 		os.Exit(1)
+	}
+	givenId := os.Args[1]
+
+	readPeers("group.txt", givenId) // Read Peer Info
+
+	api := new(MessengerAPI) // Create rpc API
+	rpc.Register(api)
+
+	listener, err := net.Listen("tcp", myID) // Start the server
+	checkError(err)
+	fmt.Println("> Service is running!")
+
+	go connectMessenger() // // Connect to Peers
+
+	for {
+		conn, err := listener.Accept() // Wait for connections
+		if err != nil {
+			continue
+		}
+		go rpc.ServeConn(conn)
 	}
 }
